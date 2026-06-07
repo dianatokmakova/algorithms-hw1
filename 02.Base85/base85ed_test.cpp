5e6d800f6751951483ed9c2d0508f55c222de598
dianatokmakova <dianatokmakova2007@gmail.com>
2026-06-07 21:48:15 +0200

    Fix AStyle formatting in 02.Base85

--- 02.Base85/base85ed_test.cpp
+++ 02.Base85/base85ed_test.cpp
@@ -6,15 +6,18 @@
 
 using namespace std;
 
-vector<uint8_t> f1(string s) {
+vector<uint8_t> f1(string s)
+{
     return vector<uint8_t>(s.begin(), s.end());
 }
 
-string f2(vector<uint8_t> v) {
+string f2(vector<uint8_t> v)
+{
     return string(v.begin(), v.end());
 }
 
-TEST(Base85, PodlyeEncode) {
+TEST(Base85, PodlyeEncode)
+{
     EXPECT_EQ(f2(base85::encode(f1(""))), "");
     EXPECT_EQ(f2(base85::encode(f1(string("\x00\x00\x00\x00", 4)))), "00000");
     EXPECT_EQ(f2(base85::encode(f1(string("\xFF\xFF\xFF\xFF", 4)))), "|NsC0");
@@ -25,7 +28,8 @@ TEST(Base85, PodlyeEncode) {
     EXPECT_EQ(f2(base85::encode(f1("ABCDE"))), "K|(`BMF");
 }
 
-TEST(Base85, PodlyeDecode) {
+TEST(Base85, PodlyeDecode)
+{
     EXPECT_EQ(f2(base85::decode(f1(""))), "");
     EXPECT_EQ(f2(base85::decode(f1("00000"))), string("\x00\x00\x00\x00", 4));
     EXPECT_EQ(f2(base85::decode(f1("|NsC0"))), string("\xFF\xFF\xFF\xFF", 4));
@@ -36,9 +40,11 @@ TEST(Base85, PodlyeDecode) {
     EXPECT_EQ(f2(base85::decode(f1("K|(`BMF"))), "ABCDE");
 }
 
-TEST(Base85, BigTest) {
+TEST(Base85, BigTest)
+{
     vector<uint8_t> v;
-    for (int i = 0; i < 15000; i++) {
+    for (int i = 0; i < 15000; i++)
+    {
         v.push_back(i % 256);
     }
     EXPECT_EQ(base85::decode(base85::encode(v)), v);
